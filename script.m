6cd59ba0147fc614e1581d0b31afb2c478ff9f54
Erika Suárez Valencia <marvinesv@gmail.com>
2014-08-15 14:21:26 -0500

    Organización de los archivos de análisis en mathematica

--- script.m
+++
@@ -1,5 +0,0 @@
-(* ::Package:: *)
-
-ivals={1,2,3,5,10,20};
-networksM3=Table[Import[StringJoin["graphs/model-v3/p10000/","network-10000-",IntegerString[ivals[[i]]],"-1000.graphml"]],{i,1,2,1}];
-Export["test.txt",Table[HistogramList[VertexDegree[networksM3[[i]]],Automatic,"PDF"],{i,1,2,1}]];
