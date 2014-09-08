bb206ce22013daf7e8f17a661fe84e0d997e6c23
Erika Suárez Valencia <marvinesv@gmail.com>
2014-09-08 10:21:10 -0500

    Modificaciones para más pruebas

--- run-exps.sh
+++ run-exps.sh
@@ -1,7 +1,4 @@
-#netlogo-headless --model ~/graphs/model-v3.nlogo --setup-file ~/graphs/experiments.xml --experiment 10-100
-netlogo-headless --model ~/graphs/model-v3.nlogo --setup-file ~/graphs/experiments.xml --experiment 50-250
-#netlogo-headless --model ~/graphs/model-v3.nlogo --setup-file ~/graphs/experiments.xml --experiment 100-1000
-netlogo-headless --model ~/graphs/model-v3.nlogo --setup-file ~/graphs/experiments.xml --experiment 500-2500
-#netlogo-headless --model ~/graphs/model-v3.nlogo --setup-file ~/graphs/experiments.xml --experiment 1000-10000
-#netlogo-headless --model ~/graphs/model-v3.nlogo --setup-file ~/graphs/experiments.xml --experiment 5000-10000
-#netlogo-headless --model ~/graphs/model-v3.nlogo --setup-file ~/graphs/experiments.xml --experiment 9000-10000
+netlogo-headless --model ~/graphs/model-v3.nlogo --setup-file ~/graphs/experiments.xml --experiment 10
+netlogo-headless --model ~/graphs/model-v3.nlogo --setup-file ~/graphs/experiments.xml --experiment 100
+netlogo-headless --model ~/graphs/model-v3.nlogo --setup-file ~/graphs/experiments.xml --experiment 1000
+
