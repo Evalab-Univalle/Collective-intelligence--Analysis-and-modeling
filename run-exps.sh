132d624b08dec34c665f21965e549b3ba8982b7e
Erika Suárez Valencia <marvinesv@gmail.com>
2014-10-31 09:56:48 -0500

    Organizando un poco el repositorio para proceder con los siguientes análisis

--- run-exps.sh
+++
@@ -1,4 +0,0 @@
-netlogo-headless --model ~/graphs/model-v3.nlogo --setup-file ~/graphs/experiments.xml --experiment 10
-netlogo-headless --model ~/graphs/model-v3.nlogo --setup-file ~/graphs/experiments.xml --experiment 100
-netlogo-headless --model ~/graphs/model-v3.nlogo --setup-file ~/graphs/experiments.xml --experiment 1000
-
